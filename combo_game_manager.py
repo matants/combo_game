84138c0503a26faac74179b3daf6e22d5a198f9b
Matan Tsipory <matan.tsipory@gmail.com>
2021-11-27 17:05:05 +0200

    added loading from excel

--- combo_game_manager.py
+++ combo_game_manager.py
@@ -1,3 +1,4 @@
+import os
 from typing import Optional
 
 import pandas as pd
@@ -7,10 +8,14 @@ class ComboGameManager:
     def __init__(self,
                  game_table_path: Optional[str] = None,
                  default_table_dir: str = "combo_data"):
+        self.default_table_dir = default_table_dir
         self.player_combo_table = self._import_game_table(game_table_path)
 
     def _import_game_table(self, game_table_path: Optional[str] = None) -> pd.DataFrame:
-
+        if game_table_path is None:
+            game_table_path = os.path.join(self.default_table_dir, max(os.listdir(self.default_table_dir)))
+        game_table = pd.read_excel(game_table_path)
+        return game_table
 
     def increase_player_combo(self):
         pass
